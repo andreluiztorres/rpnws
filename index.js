473b596e3b7444acee78affb478280eebebbc98a
Andre Luiz Torres <andre.torres.ti@outlook.com>
2021-11-20 20:34:51 -0300

    ajuste dotenv

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const LOGIN_DB = process.env.LOGIN_DB;
-const PWD_DB = process.env.PWD_DB;
+const PSW_DB = process.env.PSW_DB;
 
 // ler Json / middleware
 app.use(express.urlencoded({ extended: true }));
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 })
 
 // entregar uma porta 
-mongoose.connect(`mongodb://${LOGIN_DB}:${PWD_DB}@cluster0-shard-00-00.bsvza.mongodb.net:27017,cluster0-shard-00-01.bsvza.mongodb.net:27017,cluster0-shard-00-02.bsvza.mongodb.net:27017/dbrpn?ssl=true&replicaSet=atlas-2hligt-shard-0&authSource=admin&retryWrites=true&w=majority`)
+mongoose.connect(`mongodb://${LOGIN_DB}:${PSW_DB}@cluster0-shard-00-00.bsvza.mongodb.net:27017,cluster0-shard-00-01.bsvza.mongodb.net:27017,cluster0-shard-00-02.bsvza.mongodb.net:27017/dbrpn?ssl=true&replicaSet=atlas-2hligt-shard-0&authSource=admin&retryWrites=true&w=majority`)
 .then(() => {
     console.log("Conectado com sucesso!")
     app.listen(process.env.PORT || 3000);
